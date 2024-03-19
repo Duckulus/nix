c12efdc219b89512da83e0520d50ea66a7499725
aminh <amin.haddou@outlook.de>
2024-03-19 21:27:41 +0100

    zsh

--- nixos/home.nix
+++ nixos/home.nix
@@ -45,7 +45,7 @@
       cat = "bat";
     };
     envExtra = ''
-      export PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b ";
+      export PROMPT="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b ";
     '';
   };
 

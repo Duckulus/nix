8bac48855176fccd6af88bfc278c802ac4bd73fc
aminh <amin.haddou@outlook.de>
2024-03-19 21:25:39 +0100

    zsh

--- nixos/home.nix
+++ nixos/home.nix
@@ -46,7 +46,7 @@
     };
     envExtra = ''
       PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b ";
-    ''
+    '';
   };
 
   # Let Home Manager install and manage itself.

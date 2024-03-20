964a33dcf4765ec43a5bb5e4240da06f52e3df6f
aminh <amin.haddou@outlook.de>
2024-03-20 17:49:22 +0100

    install zoom

--- nixos/home/packages/apps.nix
+++ nixos/home/packages/apps.nix
@@ -1,14 +1,15 @@
 { pkgs, unstable, ... }: {
   home.packages = with pkgs; [
+    unstable.vesktop
     firefox
     pavucontrol
     chromium
     pinta
     dolphin
+    zoom-us
 
     neofetch
     eza
     bat
-    unstable.vesktop
   ];
 }

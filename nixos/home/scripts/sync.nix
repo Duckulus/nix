a7b5ce8644ac97d1b043f628e4709ad024e48047
aminh <amin.haddou@outlook.de>
2024-03-20 13:42:26 +0100

    sync script

--- nixos/home/scripts/sync.nix
+++ nixos/home/scripts/sync.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 
-pkgs.writeShellScriptBin "sync" ''
+pkgs.writeShellScriptBin "myscript" ''
   git -C ~/.nix add .
   git -C ~/.nix commit -m "$(${pkgs.rofi-wayland}/bin/rofi -dmenu -p "Enter commit message")"
-  git push  
+  git -C ~/.nix push  
 ''
